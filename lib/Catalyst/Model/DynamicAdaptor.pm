7a6f6abe465655a031918b14e00e5704d8eb1cc3
Taiyu Fujii <tf.900913@gmail.com>
2013-04-05 18:00:03 +0900

    Add some description for perldoc.

--- lib/Catalyst/Model/DynamicAdaptor.pm
+++ lib/Catalyst/Model/DynamicAdaptor.pm
@@ -64,3 +64,106 @@ sub BUILD {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+=head1 NAME
+
+Catalyst::Model::DynamicAdaptor - Dynamically load adaptor modules
+
+=head1 VERSION
+
+0.1
+
+=head1 SYNOPSIS
+
+ package MyApp::Web::Model::Logic;
+
+ use base qw/Catalyst::Model::DynamicAdaptor/;
+
+ __PACKAGE__->config(
+      class => 'MyApp::Logic',         # all modules under MyApp::Logic::* will be loaded
+      root  => '/path/to/MyApp/Logic', # MyApp::Logic diretcory
+   );
+
+ 1;
+
+ package MyApp::Logic;
+
+ use Moose;
+ use namespace::autoclean;
+
+ sub ACCEPT_CONTEXT {
+     my ($self, $c, @array) = @_;
+
+     # use catalyst object: $c as you like.
+
+     return $self;
+ }
+
+ sub BUILD {
+
+ .....
+
+ }
+
+ 1;
+
+ package MyApp::Logic::Foo;
+
+ use Moose;
+ use namespace::autoclean;
+
+ extends qw/MyApp::Logic/;
+
+ sub bar {
+     my $self = shift;
+
+     ....
+ }
+
+ 1;
+
+ package MyApp::Web::Controller::Foo;
+
+ sub foo : Local {
+     my ( $self, $c ) = @_;
+
+     $c->model('Logic::Foo')->bar() ;
+ }
+
+ 1;
+
+ # For not web application modules.
+
+ package MyApp::CLI;
+
+ use MyApp::Logic::Foo;
+
+ sub hoge {
+     my $self = shift;
+
+     my $logic = MyApp::Logic::Foo->new();
+     $logic->bar();
+ }
+
+ 1;
+
+=head1 DESCRIPTION
+
+ Load modules dynamicaly like L<Catalyst::Model::DBIC::Schema> does.
+
+=head1 MODULE
+
+=head2 new
+
+constructor
+
+=head1 AUTHOR
+
+Taiyu Fujii <tf.900913@gmail.com>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
